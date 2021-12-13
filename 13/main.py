6af887bcfdcb7e8fd838c31fb4f5ae80f31ad785
Ludvik Brodl <ludvik.brodl@gmail.com>
2021-12-13 23:59:13 +0100

    It's easier when you try to read your code instead of trying to fix it...

--- 13/main.py
+++ 13/main.py
@@ -35,41 +35,29 @@ def calc_visible_dots(dots, fold_insts, nbr_folds=1):
         matrix[y][x] = 1
     # print("nonzero", np.count_nonzero(matrix))
     for axis, nbr in fold_insts[:nbr_folds]:
-        print("matrix shape", matrix.shape)
-        if axis == "x":
-            new_matrix = matrix[:, :nbr]
-            to_flip_part = matrix[:, nbr + 1 :]
-            flipped = np.flip(to_flip_part, axis=1)
-            matrix = new_matrix[:, : flipped.shape[1]] + flipped
+        if axis == "y":
+            next_matrix = matrix[:nbr, :]
+            to_fold = matrix[nbr + 1 :, :]
+            flipped_to_fold = np.flip(to_fold, axis=0)
+            next_matrix[-flipped_to_fold.shape[0] :, :] += flipped_to_fold
+            matrix = next_matrix
         else:
-            new_matrix = matrix[:nbr][:]
-            to_flip_part = matrix[nbr + 1 :][:]
-            flipped = np.flip(to_flip_part, axis=0)
-            matrix = new_matrix[: flipped.shape[0]][:] + flipped
-        print("to_flip_part", to_flip_part)
-        print("flipped", flipped)
-        print("matrix", matrix)
-
-    print("matrix shape", matrix.shape)
-    # odd = 0 if nbr % 2 == 1 else 1
-    # if axis == "x":
-    #     to_fold_part = matrix[:, nbr:]
-    #     flipped = np.flip(to_fold_part, axis=1)
-    #     matrix = matrix[:, : nbr + odd] + flipped
-    # else:
-    #     to_fold_part = matrix[nbr:][:]
-    #     flipped = np.flip(to_fold_part, axis=0)
-    #     matrix = matrix[: nbr + odd][:] + flipped
-    #     # matrix =
-    return np.count_nonzero(matrix)
+            next_matrix = matrix[:, :nbr]
+            to_fold = matrix[:, nbr + 1 :]
+            flipped_to_fold = np.flip(to_fold, axis=1)
+            next_matrix[:, -flipped_to_fold.shape[1] :] += flipped_to_fold
+            matrix = next_matrix
+    return matrix
 
 
 def part1_example():
     dots, fold_insts = get_data(INPUT_EXAMPLE)
     answer = calc_visible_dots(dots, fold_insts, 1)
+    answer = np.count_nonzero(answer)
     assert answer == 17
 
     answer = calc_visible_dots(dots, fold_insts, 2)
+    answer = np.count_nonzero(answer)
     assert answer == 16
 
     return answer
@@ -78,34 +66,27 @@ def part1_example():
 def part1():
     dots, fold_insts = get_data(INPUT)
     answer = calc_visible_dots(dots, fold_insts, 1)
+    answer = np.count_nonzero(answer)
     return answer
-
-
-def part2_example():
-    return
-    # data = get_data(INPUT_EXAMPLE)
-    # answer = x(data)
-    # assert answer == 3
-
-    # return answer
 
 
 def part2():
-    return
-    data = get_data(INPUT)
-    answer = x(data)
-    return answer
+    dots, fold_insts = get_data(INPUT)
+    answer = calc_visible_dots(dots, fold_insts, len(fold_insts))
+    answer = np.where(answer > 0, 1, " ")
+    script_location = Path(__file__).absolute().parent
+    file_location = script_location / "code.txt"
+    np.savetxt(file_location, answer, fmt="%s")
+    return open(file_location, "r").read()
 
 
 def main():
     ex1 = part1_example()
-    ex2 = part2_example()
     p1 = part1()
     p2 = part2()
     print(f"example part 1: {ex1}")
     print(f"part 1: {p1}")
-    print(f"example part 2: {ex2}")
-    print(f"part 2: {p2}")
+    print(f"part 2:\n{p2}")
 
 
 if __name__ == "__main__":
